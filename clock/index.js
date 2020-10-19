d8606670d5c011a020ccaa01765569aaab4346b7
Macbook <macbook@MacBook-Pro.local>
2020-10-19 14:13:22 -0400

    search and select cities

--- clock/index.js
+++ clock/index.js
@@ -19,7 +19,7 @@ setInterval (function() {
     let minutes = currentTime.getMinutes()
     let seconds = currentTime.getSeconds()
 
-    if(hours>12)
+    if(hours+timeDiff>12)
     hours-=12
 
     document.getElementById('hours').innerHTML = hours + timeDiff
@@ -42,59 +42,193 @@ setInterval (function() {
 // hands should be accurate. use ratios of a circle
 // use clickable buttons instead of search box. more efficient unless import library
 
-function searchAtl() {
+let atl = document.getElementById('atlLabel')
+let tokyo = document.getElementById('tokyoLabel')
+let anchorage = document.getElementById('anchorageLabel')
+let hongKong = document.getElementById('hongKongLabel')
+let paris = document.getElementById('parisLabel')
+let sydney = document.getElementById('sydneyLabel')
+let moscow = document.getElementById('moscowLabel')
+let mumbai = document.getElementById('mumbaiLabel')
+
+function searchAtl() { 
     let searching = document.getElementById('searchBar').value
-    let atl = document.getElementById('atlLabel')
-    let tokyo = document.getElementById('tokyoLabel')
-    let anchorage = document.getElementById('anchorageLabel')
-    let hongKong = document.getElementById('hongKongLabel')
     
     if(searching == 'atlanta') {
+        timeDiff = 0
         atl.style.display = 'block'
         anchorage.style.display = 'none'
         hongKong.style.display = 'none'
         tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'none'
     } else if(searching == 'tokyo') {
         timeDiff = 1
         atl.style.display = 'none'
         anchorage.style.display = 'none'
         hongKong.style.display = 'none'
         tokyo.style.display = 'block'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'none'
     } else if(searching == 'anchorage') {
-        timeDiff = 4
+        timeDiff = 6
         atl.style.display = 'none'
         anchorage.style.display = 'block'
         hongKong.style.display = 'none'
         tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'none'
     } else if(searching == 'hong kong')  {
         timeDiff = 0
         atl.style.display = 'none'
         anchorage.style.display = 'none'
         hongKong.style.display = 'block'
         tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'none'
     } else if(searching == 'paris')  {
-        timeDiff = 0
+        timeDiff = 6
         atl.style.display = 'none'
         anchorage.style.display = 'none'
-        hongKong.style.display = 'block'
+        hongKong.style.display = 'none'
         tokyo.style.display = 'none'
+        paris.style.display = 'block'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'none'
     } else if(searching == 'sydney')  {
-        timeDiff = 0
+        timeDiff = 3
         atl.style.display = 'none'
         anchorage.style.display = 'none'
-        hongKong.style.display = 'block'
+        hongKong.style.display = 'none'
         tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'block'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'none'
     } else if(searching == 'moscow')  {
-        timeDiff = 0
+        timeDiff = 7
         atl.style.display = 'none'
         anchorage.style.display = 'none'
-        hongKong.style.display = 'block'
+        hongKong.style.display = 'none'
         tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'block'
+        mumbai.style.display = 'none'
     } else if(searching == 'mumbai')  {
-        timeDiff = 0
+        timeDiff = -2
         atl.style.display = 'none'
         anchorage.style.display = 'none'
-        hongKong.style.display = 'block'
+        hongKong.style.display = 'none'
         tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'block'
     }
 }
+
+function atlantaTime() {
+    timeDiff = 0
+        atl.style.display = 'block'
+        anchorage.style.display = 'none'
+        hongKong.style.display = 'none'
+        tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'none'
+}
+
+function tokyoTime() {
+    timeDiff = 1
+        atl.style.display = 'none'
+        anchorage.style.display = 'none'
+        hongKong.style.display = 'none'
+        tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'block'
+}
+
+function anchorageTime() {
+    timeDiff = 6
+        atl.style.display = 'none'
+        anchorage.style.display = 'block'
+        hongKong.style.display = 'none'
+        tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'none'
+}
+
+function hongKongTime() {
+    timeDiff = 0
+        atl.style.display = 'none'
+        anchorage.style.display = 'none'
+        hongKong.style.display = 'block'
+        tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'none'
+}
+
+function parisTime() {
+    timeDiff = 6
+    atl.style.display = 'none'
+    anchorage.style.display = 'none'
+    hongKong.style.display = 'none'
+    tokyo.style.display = 'none'
+    paris.style.display = 'none'
+    sydney.style.display = 'none'
+    moscow.style.display = 'none'
+    mumbai.style.display = 'block'
+}
+
+function sydneyTime() {
+    timeDiff = 3
+        atl.style.display = 'none'
+        anchorage.style.display = 'none'
+        hongKong.style.display = 'none'
+        tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'block'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'none'
+}
+
+function moscowTime() {
+    timeDiff = 7
+    atl.style.display = 'none'
+    anchorage.style.display = 'none'
+    hongKong.style.display = 'none'
+    tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'block'
+}
+    
+ function mumbaiTime() {
+        timeDiff = -2
+        atl.style.display = 'none'
+        anchorage.style.display = 'none'
+        hongKong.style.display = 'none'
+        tokyo.style.display = 'none'
+        paris.style.display = 'none'
+        sydney.style.display = 'none'
+        moscow.style.display = 'none'
+        mumbai.style.display = 'block'
+}   
